24d823e82b138cc32765ab2665525e27dcb106aa
My-MC <84212641+My-MC@users.noreply.github.com>
2024-10-26 23:59:18 +0900

    Setted up NextUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -14,6 +15,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
